89e4d3f71e41e20d233b6fe8e899f2da1f1811c6
Nham Le <nv3le@uwaterloo.ca>
2022-07-25 23:43:11 -0400

    Run ACAS relu pattern on all label pair

--- marabou_acas.py
+++ marabou_acas.py
@@ -3,36 +3,38 @@ This script will try to check if a ReLU pattern is good enough to prove the outc
 
 """
 
+import json
 import re
 from typing import Set, Tuple, List
 import numpy as np
 from maraboupy import Marabou, MarabouCore, MarabouUtils
+import logging
+import pandas
 
+from torch import double
 
 PATH = '/home/nle/workspace/VerifyNNE/datasets/ACAS/acas_nets/ACASXU_run2a_1_3_batch_2000.nnet'
 EPSILON = 0.005
+PATTERN_PATH = '/home/nle/workspace/VerifyNNE/datasets/ACAS/relu_patterns.json'
+MAX_TIME = 300 #in seconds
+M_OPTIONS: MarabouCore.Options = Marabou.createOptions(verbosity=0,
+                                  timeoutInSeconds=MAX_TIME
+                                  )
 
 
-relu_check_list = [1, 2, 20, 27, 29, 30, 43, 59, 61, 63, 65, 69, 73, 87, 96, 99, 104, 106, 132, 141, 145, 148, 152, 153, 164, 165, 166, 174, 175, 176, 178, 179, 182, 185, 186, 188, 192, 201, 205, 207, 208, 211, 220, 221, 223, 226, 232, 243]
-relu_val = [2000, 0, 0, 0, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2000, 0, 0, 2000, 0, 0, 0, 2000, 0, 0, 0, 0, 2000, 2000, 0, 0, 0, 2000, 0, 0, 0, 0, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2000, 2000]
+with open(PATTERN_PATH, "r") as f:
+    STABLE_PATTERNS = json.load(f)
 
-# relu_check_list = [1, 2, 19, 20, 27, 29, 30, 43, 59, 61, 63, 65, 69, 73, 87, 96, 99, 104, 106, 114, 132, 141, 145, 148, 151, 152, 153, 164, 165, 166, 172, 174, 175, 176, 178, 179, 180, 182, 184, 185, 186, 188, 192, 201, 202, 205, 207, 208, 210, 211, 213, 218, 220, 221, 223, 226, 232, 241, 243]
-# relu_val = [2000, 0, 8, 0, 0, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2000, 0, 0, 2000, 1, 0, 0, 0, 2000, 5, 0, 0, 0, 0, 2000, 8, 2000, 0, 0, 0, 2000, 1990, 0, 4, 0, 0, 0, 2000, 0, 7, 0, 0, 0, 1, 0, 1, 5, 0, 0, 0, 0, 2000, 1997, 2000]
 
-#relu_check_list = [2, 27, 29, 30, 63, 65, 69, 73, 87, 99, 104, 132, 148, 165, 179, 188, 207, 208, 232]
-#relu_val = [0, 0, 2000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2000, 0, 2000, 0, 0, 0, 2000]
-
-assert(len(relu_check_list)==len(relu_val))
-
-def init_network():
-    network = Marabou.read_nnet(PATH)
+def init_network()->Marabou.MarabouNetworkNNet:
+    network:Marabou.MarabouNetworkNNet = Marabou.read_nnet(PATH)
 
     print("output nodes:", network.outputVars)
     print("input nodes:", network.inputVars)
-    print("relu list:")
 
+    logging.debug("relu list:")
     for r in network.reluList:
-        print(r)
+        logging.debug(r)
     network.setLowerBound(0, -1)
     network.setUpperBound(0, 1)
     network.setLowerBound(1, -1)
@@ -49,9 +51,11 @@ def init_network():
 def add_relu_constraints(network: Marabou.MarabouNetworkNNet, 
                         relu_check_list: List[int], 
                         relu_val: List[int])->Marabou.MarabouNetworkNNet:
+    """
+    Add stable relus constraints to the Marabou network
+    """
     for i in range(len(relu_check_list)):
         layer, idx, marabou_idx = parse_raw_idx(relu_check_list[i])
-        print(layer, idx, marabou_idx)
         if relu_val[i] == 0:
             constraint = MarabouUtils.Equation(MarabouCore.Equation.LE)
             constraint.addAddend(1, marabou_idx)
@@ -84,30 +88,62 @@ def find_one_assignment(relu_check_list: List[int], relu_val: List[int])->None:
         marabou_idx = parse_raw_idx(r)[-1]
         print(marabou_idx, vals[marabou_idx], relu_val[idx])
 
-def check_pattern(relu_check_list: List[int], relu_val: List[int], label: int, other_label: int)->None:
-    print("--------CHECK PATTERN--------")
+def check_pattern(relu_check_list: List[int], relu_val: List[int], label: int, other_label: int)->Tuple[str, int]:
+    """
+    In ACAS, the prediction is the label with smallest value.
+    So we check that label - other_label < 0 forall input
+    by finding assignments for label - other_label >=0
+    """
+    print("--------CHECK PATTERN: output_{} is always less than output_{} ? --------".format(label, other_label))
     network = init_network()
     network = add_relu_constraints(network, relu_check_list, relu_val)    
     offset = network.outputVars[0][0][0]
-    print("offset", offset)
+
     #add output constraint
-    # logit of 0 - logit of 1 > 0
     constraint = MarabouUtils.Equation(MarabouCore.Equation.GE)
     constraint.addAddend(1, label+offset)
     constraint.addAddend(-1, other_label+offset)
-    constraint.setScalar(0.0002)
+    constraint.setScalar(-0.0001)
     network.addEquation(constraint)
 
-    exitCode, vals, stats = network.solve()
+    try:
+        exit_code: str    
+        exit_code, vals, stats = network.solve(options=M_OPTIONS)
+        running_time:int = stats.getTotalTimeInMicro()
+        for idx, r in enumerate(relu_check_list):
+            marabou_idx = parse_raw_idx(r)[-1]
+            print(marabou_idx, vals[marabou_idx], relu_val[idx])
 
-    for idx, r in enumerate(relu_check_list):
-        marabou_idx = parse_raw_idx(r)[-1]
-        print(marabou_idx, vals[marabou_idx], relu_val[idx])
+        print("double check output")
+        for o in network.outputVars[0][0]:
+            print(o, vals[o])
+        print(label+offset, vals[label+offset])
+        print(other_label+offset, vals[other_label+offset])
 
-    print("double check output")
-    for o in network.outputVars[0][0]:
-        print(o, vals[o])
-    print(label+offset, vals[label+offset])
-    print(other_label+offset, vals[other_label+offset])
-# find_one_assignment(relu_check_list, relu_val)
-check_pattern(relu_check_list, relu_val, label=0, other_label = 1)
+        print("Running time:{}".format(running_time))
+        return exit_code, running_time
+    except Exception:
+        if exit_code not in ["sat", "unsat"]:
+            print("THE QUERY CANNOT BE SOLVED")
+        return exit_code, -1
+def main():
+    res = [[-1]*5 for i in range(5)]
+    print(res)
+    for label in STABLE_PATTERNS:
+        print("For label {}, check if its stable RELU pattern guarantees the output")
+        for other_label in range(5):
+            if other_label == int(label):
+                continue
+            relu_check_list = STABLE_PATTERNS[label]["stable_idx"]
+            relu_val = STABLE_PATTERNS[label]["val"] 
+            exit_code, running_time = check_pattern(relu_check_list, relu_val, label=int(label), other_label = other_label)
+            if exit_code=="sat":
+                res[int(label)][other_label] = "SAT:{}".format(running_time/10**6)
+            elif exit_code=="unsat":
+                res[int(label)][other_label] = "UNS:{}".format(running_time/10**6)
+            else:
+                res[int(label)][other_label] = exit_code
+
+    res = pandas.DataFrame(res)
+    print(res)
+main()
