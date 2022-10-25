d5f2fb46690d3f362edfbac6dec044583eb9d8ce
Nham Le <nv3le@uwaterloo.ca>
2022-10-25 00:41:12 -0400

    the parsing script seems to be working

--- parse_marabou_output.py
+++ parse_marabou_output.py
@@ -2,6 +2,9 @@ import string
 from lark import Lark, Transformer, v_args
 INPUT='Marabou_output.txt'
 
+"""
+Grammar and parser for parsing the assignments
+"""
 value_grammar = '''
     start: varname "(index:" index ")" "-->" value "[" vartype "]." "Range:" "[" lower "," upper "]" -> assign_var
     varname: CNAME
@@ -18,8 +21,6 @@ value_grammar = '''
 '''
 @v_args(inline=True)
 class VariableAssignment(Transformer):
-    def __init__(self):
-        self.vars = {}
     varname = str
     vartype = str
     index = int
@@ -28,7 +29,56 @@ class VariableAssignment(Transformer):
     upper = float
     def assign_var(self, varname, index, value, vartype, lower, upper):
         return varname, {"index": index, "value": value, "vartype": vartype, "bounds": [lower, upper]}
-parser = Lark(value_grammar, parser='lalr', transformer = VariableAssignment())
+var_parser = Lark(value_grammar, parser='lalr', transformer = VariableAssignment())
+
+"""
+Grammar and parser for parsing the equations
+"""
+eq_grammar = '''
+    start: equation+ ->assign_all_eqs
+
+    equation: linear scalar lhs ->assign_equation
+
+    linear: varname "=" term* ->assign_linear
+    term: coeff "*" varname "," ->assign_term
+    
+    scalar: "scalar = " value ->assign_scalar
+
+    lhs: "lhs = " varname ->assign_lhs 
+    coeff: SIGNED_NUMBER
+    varname: CNAME
+    value: SIGNED_NUMBER
+    %import common.CNAME
+    %import common.NEWLINE
+    %import common.SIGNED_NUMBER
+    %import common.WS
+    %ignore WS           // Disregard spaces in text
+'''
+@v_args(inline=True)
+class EqAssignment(Transformer):
+    varname = str
+    coeff = float
+    value = float 
+    def assign_all_eqs(self, *equation):
+        return equation
+
+    def assign_equation(self, linear, scalar, lhs):
+        return linear, scalar, lhs
+
+    def assign_scalar(self, value:float):
+        return value
+
+    def assign_term(self, coeff, varname):
+        return coeff, varname
+
+    def assign_linear(self, varname, *terms):
+        all_terms = []        
+        for t in terms:
+            all_terms.append(t)
+        return all_terms
+    def assign_lhs(self, varname):
+        return varname
+eq_parser = Lark(eq_grammar, parser='lalr', transformer = EqAssignment())
 
 with open(INPUT, "r") as f:
     raw = f.readlines()
@@ -45,17 +95,29 @@ for idx, l in enumerate(raw):
     if l.startswith("Dumping tableau equations") and idx > start_values_idx:
         end_values_idx = idx
         start_eq_idx = idx+1
-    if l.startswith("Engine::solve: unsat query"):
+    if l.startswith("Engine: Linear constraints") or l.startswith("Engine::solve: unsat") and idx>start_eq_idx:
         end_eq_idx = idx
 values_lines=raw[start_values_idx:end_values_idx]
 eq_lines = raw[start_eq_idx:end_eq_idx]
 values = {}
 for l in values_lines:
-    # print(l)
-    varname, attributes = parser.parse(l.strip())
+    print(l)
+    varname, attributes = var_parser.parse(l.strip())
     values[varname] = attributes
 
 print(values)
 
-for l in eq_lines:
-    print(l)
+eq_raws = "".join(eq_lines)
+print(eq_raws)
+
+all_eqs = eq_parser.parse(eq_raws)
+
+#compute eqs using vals
+for eq in all_eqs:
+    rhs, bias, lhs = eq
+    print("computing", rhs, bias)
+    rhs_val = 0
+    for term in rhs:
+        rhs_val += term[0]*values[term[1]]["value"]
+    rhs_val+=bias
+    print(rhs_val, lhs, values[lhs]["value"]) 
