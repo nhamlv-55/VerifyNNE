07d1b7951e05db57972100d6f96c38fa899d7f13
Nham Le <nhamlevan@gmail.com>
2022-11-08 17:00:22 -0500

    add output constraint

--- CIFAR10_augmented_robustness.py
+++ CIFAR10_augmented_robustness.py
@@ -61,20 +61,24 @@ assert len(marabou_network.reluList) == len(bitmap)
 
 input = np.load(IMAGE_PATH).flatten()
 print(input[:10])
-
+output = marabou_network.outputVars[0][0]
+print(output)
 #set range for input
 for i in range(len(input)):
     marabou_network.setLowerBound(i, max(0, input[i]-0.012))
     marabou_network.setUpperBound(i, min(1, input[i]+0.012))
-
+#add output constraint
+constraint = MarabouUtils.Equation(MarabouCore.Equation.GE)
+constraint.addAddend(1, output[1]) #adv label
+constraint.addAddend(-1, output[0]) #true label
+constraint.setScalar(0)
+marabou_network.addEquation(constraint)
 #set relu constraints
 #for any relu that is fixed, we need to remove it from the reluList.
 new_relu_list = []
 for idx, relu_inout in enumerate(marabou_network.reluList):
     relu_in, relu_out = relu_inout
-    print(relu_in, relu_out, bitmap[idx])
     if bitmap[idx]=="1": #positive relu
-        print("setting positive phase")
         #input of relu must be >=0
         marabou_network.setLowerBound(relu_in, 0)
         #output of relu must be relu_in
@@ -85,7 +89,6 @@ for idx, relu_inout in enumerate(marabou_network.reluList):
         marabou_network.addEquation(positive_phase_con)
 
     elif bitmap[idx]=="0": #negative relu
-        print("setting negative phase")
         #output of relu must be 0
         marabou_network.setLowerBound(relu_out, 0)
         marabou_network.setUpperBound(relu_out, 0)
@@ -104,38 +107,3 @@ exit_code, vals, stats = marabou_network.solve(options=M_OPTIONS)
 print(exit_code)
 print(json.dumps(vals, indent=2))
 exit(0)
-
-# %%
-"""write the relu values out"""
-print(len(outputDict))
-print(len(marabou_network.reluList))
-relu_dict = {"pred_label": int(pred_label), "true_label": int(true_label), "relu_dict": {}}
-for i, o in marabou_network.reluList:
-    relu_dict["relu_dict"][str(i)] = outputDict[i]
-res = {"outputDict": outputDict, "reluList": marabou_network.reluList}
-np.save(IMAGE_PATH, input)
-with open(RELUDICT_PATH, "w") as f:
-    json.dump(relu_dict, f, indent=2)
-with open(RELUBITMAP_PATH, "w") as f:
-    res_str = ""
-    for i, o in marabou_network.reluList:
-        if outputDict[i] <=0: res_str+="0"
-        else: res_str+="1"
-    f.write("{} {}\n".format(pred_label, true_label))
-    f.write(res_str)
-
-
-
-# %%
-""" Check if the written files are correct """
-with open(RELUDICT_PATH, "r") as f:
-    loaded_relu_dict = json.load(f)["relu_dict"]
-with open(RELUBITMAP_PATH, "r") as f:
-    relu_map = f.readlines()[1].strip()
-
-assert len(relu_map) == len(loaded_relu_dict.keys()), print(len(relu_map), "!=", len(loaded_relu_dict.keys()))
-relu_keys = sorted(loaded_relu_dict.keys())
-for ridx, relu in enumerate(relu_keys):
-    assert int(loaded_relu_dict[relu] >0) == int(relu_map[ridx]), print(relu, loaded_relu_dict[relu]>0, ridx, relu_map[ridx])
-
-
