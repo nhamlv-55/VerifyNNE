ae3073364dca86182f880f5284c8c04aeb6efee5
Nham Le <nv3le@uwaterloo.ca>
2023-02-06 15:42:41 +0000

    run experiment for mnist_fc256x6

--- NAP_augmented_robustness.py
+++ NAP_augmented_robustness.py
@@ -9,36 +9,43 @@ import torchvision
 from maraboupy import Marabou, MarabouCore, MarabouUtils
 from utils import load_vnnlib
 import datetime
-PATH = 'vnncomp2021/benchmarks/mnistfc/mnist-net_256x4.onnx'
+PATH = 'vnncomp2021/benchmarks/mnistfc/mnist-net_256x6.onnx'
 MAX_TIME = 600  # in seconds
 
 M_OPTIONS: MarabouCore.Options = Marabou.createOptions(verbosity=0,
-                                                       initialSplits=2,
+                                                       initialSplits=4,
                                                        timeoutInSeconds=MAX_TIME,
                                                        snc=True,
-                                                       numWorkers=16,
+                                                       numWorkers=32,
                                                        )
 
 
-RESULT_CSV = open("MNIST_ROBUST.csv", 'a')
+RESULT_CSV = open("MNIST_ROBUST_256x6.csv", 'a')
 # load benchmarks from vnncomp
-BENCHMARK_PATH = 'fig1.json'
-RAW = json.load(open(BENCHMARK_PATH, "r"))
-TARGET_INPUT = []
-for r in RAW["target"]:
-    TARGET_INPUT.extend(r)
-print(len(TARGET_INPUT))
+# BENCHMARK_PATH = 'fig1.json'
+# RAW = json.load(open(BENCHMARK_PATH, "r"))
+# TARGET_INPUT = []
+# for r in RAW["target"]:
+#     TARGET_INPUT.extend(r)
+# print(len(TARGET_INPUT))
+
+BENCHMARK_PATH = 'datasets/MNIST/prop_7_0.05.vnnlib'
+input, eps, true_label, adv_label = load_vnnlib(BENCHMARK_PATH)
+print("true label", true_label)
+print("eps", eps)
+TARGET_INPUT = input.numpy()
+print(TARGET_INPUT[:10])
 
 BENCHMARK_FOLDER = 'vnncomp2021/benchmarks/mnistfc'
 
-PATTERN_PATH = 'datasets/MNIST/mnist_relu_patterns_0.json'
+PATTERN_PATH = 'datasets/MNIST/MNIST_256x6_relu_patterns_0.json'
 with open(PATTERN_PATH, "r") as f:
     STABLE_PATTERNS = json.load(f)
 
 
 def add_relu_constraints(network: Marabou.MarabouNetwork,
                          relu_check_list: List[int],
-                         relu_val: List[int]) -> Marabou.MarabouNetwork:
+                         relu_val: List[int]) -> None:
     """
     Add stable relus constraints to the Marabou network
     """
@@ -47,26 +54,34 @@ def add_relu_constraints(network: Marabou.MarabouNetwork,
         # print(layer, idx, marabou_idx, marabou_idx+256)
         # print(relu_val[i])
         if relu_val[i] < 2000 : #should be correct all the way to delta =0.5
+            constraint = MarabouUtils.Equation(MarabouCore.Equation.LE)
+            constraint.addAddend(1, marabou_idx)
+            constraint.setScalar(0)
+
+
             #output of ReLU must be 0
             network.setLowerBound(marabou_idx+256, 0)
             network.setUpperBound(marabou_idx+256, 0)
 
             #input of ReLU must be <=0
-            network.setUpperBound(marabou_idx, 0)
+            # network.setUpperBound(marabou_idx, 0)
 
         else:
             #input of ReLU must be >=0
-            network.setLowerBound(marabou_idx, 0)
+            constraint = MarabouUtils.Equation(MarabouCore.Equation.GE)
+            constraint.addAddend(1, marabou_idx)
+            constraint.setScalar(0)
+            # network.setLowerBound(marabou_idx, 0)
 
             #output of relu must be the same as input
-            positive_phase_con = MarabouUtils.Equation(MarabouCore.Equation.EQ)
-            positive_phase_con.addAddend(1, marabou_idx)
-            positive_phase_con.addAddend(-1, marabou_idx+256)
-            positive_phase_con.setScalar(0)
-            print(positive_phase_con)
-            network.addEquation(positive_phase_con)
-
-    return network
+            # positive_phase_con = MarabouUtils.Equation(MarabouCore.Equation.EQ)
+            # positive_phase_con.addAddend(1, marabou_idx)
+            # positive_phase_con.addAddend(-1, marabou_idx+256)
+            # positive_phase_con.setScalar(0)
+            # print(positive_phase_con)
+            # network.addEquation(positive_phase_con)
+        network.addEquation(constraint)
+    # # return network
 
 
 def check_pattern(network: Marabou.MarabouNetwork, prop_name: str,
@@ -75,7 +90,7 @@ def check_pattern(network: Marabou.MarabouNetwork, prop_name: str,
     print("--------CHECK PATTERN: output_{} is always less than output_{} ? --------".format(label, other_label))
     print("number of fixed relus:", len(relu_check_list))
     # network = init_network()
-    network = add_relu_constraints(network, relu_check_list, relu_val)
+    add_relu_constraints(network, relu_check_list, relu_val)
     offset = network.outputVars[0][0][0]
     print(offset)
     # add output constraint
@@ -88,13 +103,11 @@ def check_pattern(network: Marabou.MarabouNetwork, prop_name: str,
         constraint.setScalar(0.00001)
 
         network.addEquation(constraint)
-    # return 
+    network.saveQuery("testQuery")
     try:
         print("start solving")
         exit_code, vals, stats = network.solve( filename="{}_{}_vs_{}".format(prop_name, label, other_label),
                                                 options=M_OPTIONS, 
-                                                fixed_relu_list = [parse_raw_idx(idx)[2] for idx in relu_check_list]
-                                                # fixed_relu_list = []
                                                 )
         running_time: int = stats.getTotalTimeInMicro()
         if exit_code=="sat":
@@ -120,7 +133,7 @@ def check_pattern(network: Marabou.MarabouNetwork, prop_name: str,
 
 def parse_raw_idx(raw_idx: int) -> Tuple[int, int, int]:
     """
-    only for MNIST 256x4 network:
+    only for MNIST 256xk network:
     """
     n_relus = 256
     offset = 28*28+10
@@ -131,22 +144,30 @@ def parse_raw_idx(raw_idx: int) -> Tuple[int, int, int]:
 
 
 def run_benchmark(benchmark: str):
+    global true_label
     RESULT_CSV.write(benchmark+"\n")
     RESULT_CSV.flush()
     table = ["UNK"]*10
     network_path = PATH
     x = TARGET_INPUT
-    eps = RAW["epsilon"]
-    true_label = 1
-    
+    eps = 0.05
+    true_label = true_label
+    fig = plt.figure
+    plt.imshow(x.reshape(28, 28), cmap='gray')
+    plt.savefig("Prop.png")   
     for adv_label in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        if adv_label == true_label: continue
+        RESULT_CSV.write("vs {}".format(adv_label))
+        RESULT_CSV.flush() 
         network = Marabou.read_onnx(network_path)
+        print(network.numVars)
         # check if the benchmarks are really violated
-        checking_eps = 0.2
+        checking_eps = eps
         print("Checking with eps = {}".format(checking_eps))
+        print("len x", len(x))
         for i in range(len(x)):
-            network.setLowerBound(i, max(0, x[i]-eps) )
-            network.setUpperBound(i, min(1, x[i]+eps))
+            network.setLowerBound(i, max(0, x[i]-checking_eps) )
+            network.setUpperBound(i, min(1, x[i]+checking_eps))
             #check a ball centered at vector 0
             # network.setLowerBound(i, 0.5 - checking_eps)
             # network.setUpperBound(i, 0.5 + checking_eps)
@@ -169,11 +190,9 @@ def run_benchmark(benchmark: str):
         RESULT_CSV.flush()
         print(exit_code, running_time, my_time)
     print(table)
-        # fig = plt.figure
-        # plt.imshow(x.resize(28, 28), cmap='gray')
-        # plt.savefig(prop_path+".png")
+
 
 def main():
-    violated = run_benchmark("")
+    violated = run_benchmark("prop_7_0.05.vnnlib")
 
 main()
