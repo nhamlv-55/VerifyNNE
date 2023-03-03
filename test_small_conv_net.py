4f096877902ef658b403e073e0c1bd948467156f
Nham Le <nv3le@uwaterloo.ca>
2023-03-03 18:31:34 +0000

    even smaller conv_net

--- test_small_conv_net.py
+++ test_small_conv_net.py
@@ -12,15 +12,14 @@ import numpy as np
 from utils import set_default_bound, _write
 torch.set_default_tensor_type(torch.DoubleTensor)
 
-W1 =[[[[.101, .102, .103],
-      [.111, -.112, .113],
-      [.121, .122, .123]
+W1 = [[[[.101, .102],
+        [.111, -.112]
        ]],
-      [[[-.201, -.202, .203],
-      [.211, -.212, .213],
-      [.221, .222, -.223]]
+      [[[-.201, -.202],
+        [.211, -.212]
+      ]
       ]]
-B1 = [0.01, -0.095 ]
+B1 = [0.01, -0.095]
 W2 = [[.301, .302, -.303, .304, -.305, .306, .307, -.308],
       [.311, .312, .313, -.314, .315, .316, -.317, -.318]]
 B2 = [0.007, -0.1]
@@ -31,7 +30,8 @@ W3 = [[.301, .302],
 class ToyConvNet(BaseNet):
     def __init__(self):
         super().__init__()
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3)
+        self.conv1 = torch.nn.Conv2d(
+            in_channels=1, out_channels=2, kernel_size=2)
         self.ln1 = torch.nn.Linear(8, 2, bias=True)
         self.relu = torch.nn.ReLU()
         # fixed weight
@@ -70,14 +70,12 @@ def build_saliency_mask_query(network: BaseNet, dummy_input: torch.Tensor, input
                 network.fw_ipq.setUpperBound(v, input[0][0][rid][vid].item())
     Marabou.saveQuery(network.fw_ipq, "forwardQuery_smallConv")
 
-
     """
     Setup full network (forward + backward)
     """
     set_default_bound(ipq, range(marabou_net.numVars), -2, 2)
     set_default_bound(ipq, range(marabou_net.numVars,
                       marabou_net.numVars*2), -2, 2)
-
 
     print("inputVars", marabou_net.inputVars[0])
     print("outputVars", marabou_net.outputVars[0])
@@ -106,10 +104,10 @@ def build_saliency_mask_query(network: BaseNet, dummy_input: torch.Tensor, input
 def run_toy():
     toy = ToyConvNet()
     input = torch.autograd.Variable(torch.Tensor(
-        [[[[0.11, 0.12, 0.13, 0.14],
-        [0.21, 0.22, 0.23, 0.24],
-        [0.31, 0.32, 0.33, 0.34],
-        [0.41, 0.42, 0.43, 0.44]]]]), requires_grad=True)
+        [[[[0.11, 0.12, 0.13],
+           [0.21, 0.22, 0.23],
+           [0.31, 0.32, 0.33],
+        ]]]), requires_grad=True)
     print("calculate using pytorch")
     print(input.shape)
     z1 = toy.conv1(input)
@@ -140,7 +138,7 @@ def run_toy():
         _write(z2, f)
         _write(z2.grad, f)
     ipq = build_saliency_mask_query(
-        toy, dummy_input=torch.randn(1, 1, 4, 4), input=input, target=0)
+        toy, dummy_input=torch.randn(1, 1, 3, 3), input=input, target=0)
     MarabouCore.saveQuery(ipq, "finalQuery_smallConv")
 
 run_toy()
